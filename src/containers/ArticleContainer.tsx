1f4b624e9885a63fde0b1aea7cebf208f087a735
vladf29 <warvlad822@gmail.com>
2019-08-23 18:06:25 +0300

    [UPDATE] redirect after succesed removed

--- src/containers/ArticleContainer.tsx
+++ src/containers/ArticleContainer.tsx
@@ -1,27 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { withRouter, RouteComponentProps } from "react-router-dom";
 
 import { INews } from "../interfaces/INews";
 import { IApplicationState } from "../reducers";
-import { getNews } from "../actions/newsAction";
+import { getNews, deleteNews } from "../actions/newsAction";
 
 import NewsHeader from "../components/NewsHeader";
 import NewsContent from "../components/NewsDesc";
 
 export interface IArticaleContainerProps extends RouteComponentProps {}
 
-const ArticleContainer: React.FC<IArticaleContainerProps> = ({ match }) => {
-  const params: any = match.params;
-
+const ArticleContainer: React.FC<IArticaleContainerProps> = ({
+  match,
+  history
+}) => {
   const news: INews = useSelector(
     (state: IApplicationState) => state.news.news
   );
 
-  const dispatch = useDispatch();
+  const dispatch: any = useDispatch();
+
+  const deleteNewsClb = useCallback(async (id: string) => {
+    const res: boolean = await dispatch(deleteNews(id));
+    if (res) history.goBack();
+  }, []);
 
   useEffect(() => {
+    const params: any = match.params;
     if (news.id === params.newsId) return;
 
     dispatch(getNews(params.newsId));
@@ -34,7 +41,9 @@ const ArticleContainer: React.FC<IArticaleContainerProps> = ({ match }) => {
         author={news.author}
         date={news.date}
         id={news.id}
+        onDeleteNews={deleteNewsClb}
         showInformation
+        showControls
       />
       <NewsContent content={news.content} />
     </>
